fb565f250553a1b0db9f435df3b5a8a5c206d80b
Matt Newville <newville@cars.uchicago.edu>
2009-12-02 21:35:02 +0000

    added libifeffit.so, changed python config script

--- src/lib/sys.h
+++ src/lib/sys.h
@@ -1,9 +1,9 @@
-c{sys.h -*-fortran-*-
-c all system and build specific stuff goes here
-c to be executed in iff_config.f
-       sfile  = 'C:/Program Files/Ifeffit'
-       pgdev  = '/gw'
-       inifile= 'startup.iff  ifeffit.ini'
-       build = '1.2.11d(win)'//
-     $   ' Copyright (c) 2002 Matt Newville, Univ of Chicago '
+c{sys.h  -*-fortran-*- 
+c system and build specific stuff goes here
+c to be included in iff_config.f
+       sysdir = '/usr/local/share/ifeffit'
+       pgdev  = '/xserve'
+       inifile= 'startup.iff  .ifeffit'
+       build = '	1.2.11c'//
+     $   ' Copyright (c) 2005 Matt Newville, Univ of Chicago'
 c}
